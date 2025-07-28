d1644709478bcaff1dba65c89e7bd2e918cac4ce
Priyam Tiwari <priyamtiwari9621@gmail.com>
2025-07-28 18:49:43 +0530

    Fix Render deployment

--- indra_dental_clinic/settings.py
+++ indra_dental_clinic/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['Indra-Clinic.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = 
+[
+    'Indra-Clinic.onrender.com',
+    'localhost', '127.0.0.1'
+    os.getenv('RENDER_EXTERNAL_HOSTNAME'),
+    ]
 
 
 # Application definition
